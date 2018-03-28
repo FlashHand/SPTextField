317e845d5c6e6d39c25310fa72354e74cd972428
BoWang <ralwayne@163.com>
2018-03-28 16:06:39 +0800

    add readme

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # SPTextField
+```
+UITextField *accountField=[SPTextField spTextFieldWithWidth:200 placeholder:@"手机号码" delegate:self];
+[accountField.textField setDelegate:self];
+[accountField setRegexStr:@"^((1[3-9]))\\d{9}$"];
+[accountField.defaultSeparator setHidden:NO];
+[accountField setBlankErrStr:@"请输入手机号码"];
+[accountField setFormatErrStr:@"请输入正确的手机号码"];
+[accountField setFrame:CGRectMake(0, 20, accountField.frame.size.width, accountField.frame.size.width)];
+[self.view addSubview:accountField];
+```
--- SPTextFieldDemo/SPTextFieldDemo/ViewController.m
+++ SPTextFieldDemo/SPTextFieldDemo/ViewController.m
@@ -27,13 +27,14 @@
     [accountField.defaultSeparator setHidden:NO];
     [accountField setBlankErrStr:@"请输入手机号码"];
     [accountField setFormatErrStr:@"请输入正确的手机号码"];
+    [accountField setFrame:CGRectMake(0, 20, accountField.frame.size.width, accountField.frame.size.width)];
+    [self.view addSubview:accountField];
     passwordField=[SPTextField spTextFieldWithWidth:200 placeholder:@"密码" delegate:self];
     [passwordField.textField setDelegate:self];
     [passwordField setRegexStr:@"((?=\\w*\\d)(?=\\w*\\D)|(?=\\w*[a-zA-Z])(?=\\w*[^a-zA-Z]))^\\w{6,20}$"];
     [passwordField setBlankErrStr:@"请输入密码"];
     [passwordField setFormatErrStr:@"请输入正确的密码"];
-    [self.view addSubview:accountField];
-    [accountField setFrame:CGRectMake(0, 20, accountField.frame.size.width, accountField.frame.size.width)];
+   
     [self.view addSubview:passwordField];
     [passwordField setFrame:CGRectMake(0, 100, passwordField.frame.size.width, passwordField.frame.size.width)];
     [passwordField.textField setSecureTextEntry:YES];
